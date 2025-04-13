53717353b1c26a891d1ba40be220d083cce88dc1
venkatesh <ragipativenkateshh@gmail.com>
2025-04-13 16:08:12 +0530

    removed extra folders

--- src/main/java/com/spring/rest_api/career_crafter/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.spring.rest_api.career_crafter.enums;
-
-public enum Role {
-	HR, EXECUTIVE, ADMIN,JOBSEEKER, INSTRUCTOR
-
-}
--- src/main/java/com/spring/rest_api/career_crafter/repository/CourseModuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.rest_api.career_crafter.repository;
-
-import com.spring.rest_api.career_crafter.model.CourseModule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseModuleRepository extends JpaRepository<CourseModule, Integer> {
-}
